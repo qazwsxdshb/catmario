1c7a3fc3c0cec82d5a236b31f15af70a505021b3
easn <a35744830@gmail.com>
2025-03-25 11:52:21 +0800

    edit .gitignore

---
+++ cmake-build-debug/_deps/sdl2_image-build/CMakeFiles/SDL2_image.dir/IMG_WIC.c.o.d
@@ -0,0 +1,2 @@
+_deps/sdl2_image-build/CMakeFiles/SDL2_image.dir/IMG_WIC.c.o: \
+  /Users/easn/code/catmario/PTSD/lib/sdl2_image/IMG_WIC.c
---
+++ cmake-build-debug/_deps/sdl2_image-build/CMakeFiles/SDL2_image.dir/IMG_tif.c.o.d
@@ -0,0 +1,2 @@
+_deps/sdl2_image-build/CMakeFiles/SDL2_image.dir/IMG_tif.c.o: \
+  /Users/easn/code/catmario/PTSD/lib/sdl2_image/IMG_tif.c
